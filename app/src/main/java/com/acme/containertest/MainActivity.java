2e618fcc6d139caf2bc83c8788b0f2eef878f42e
alfredomrh <alfredomrh@hotmail.com>
2017-02-01 23:55:18 +0100

    Uno

--- app/src/main/java/com/acme/containertest/MainActivity.java
+++ app/src/main/java/com/acme/containertest/MainActivity.java
@@ -1,26 +1,25 @@
 package com.acme.containertest;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
 
     /**
+     * Primer commit AMRH
+     *
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
@@ -32,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * The {@link ViewPager} that will host the section contents.
+     *
      */
     private ViewPager mViewPager;
 
